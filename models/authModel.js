d3d566ad8b1895b76fb06159a4442a8cca179ce4
nirch <nirh2@yahoo.com>
2026-07-29 09:43:17 +0300

    using bcrypt to compare passwords

--- models/authModel.js
+++ models/authModel.js
@@ -1,24 +1,27 @@
+const bcrypt = require("bcrypt");
+
 const users = [
   {
     id: "1",
     name: "Nir Channes",
     email: "nirch@example.com",
-    password: "123",
+    password: "$2a$10$s7ryn.PiuvzZBQNhtqrts.iiC2Z5xOvPOudbZZBbPsZXu8B47ZKnS",
   },
   {
     id: "2",
     name: "John Doe",
     email: "johnd@example.com",
-    password: "123",
+    password: "$2a$10$xgWrV3IdpS6Te05vlZ8RDulCIoovt8LAfkIVvOIRovCYDSFEvY7wm",
   },
 ];
 
 async function login(email, password) {
-  const user = users.find(
-    (user) => user.email === email && user.password === password
-  );
+  const user = users.find((user) => user.email === email);
 
-  return user;
+  if (user && bcrypt.compare(password, user.password)) {
+    const { password: _, ...userNoPassword } = user;
+    return userNoPassword;
+  }
 }
 
 module.exports = { login };
